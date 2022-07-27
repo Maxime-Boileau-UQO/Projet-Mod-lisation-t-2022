03c1fa0d3fe12770f035ac148deeb23d1a7908fe
JPP44 <97757145+JPP44@users.noreply.github.com>
2022-07-27 09:17:04 -0400

    Petite corrections et set up du projet

--- src/main/java/com/example/Personne.java
+++ src/main/java/com/example/Personne.java
@@ -1,5 +1,9 @@
-package main.java.com.example;
+package com.example;
 
 public class Personne {
     
+    //Methodes statiques(peu y acceder sans instancier un objet de cette classe) 
+    public static void addPersonneToJson(){
+
+    }
 }
--- src/main/java/com/example/Solde.java
+++ src/main/java/com/example/Solde.java
@@ -1,5 +1,10 @@
-package main.java.com.example;
+package com.example;
 
 public class Solde {
     
+    //Identifint (id) du solde = nom d'utilsateur associer au solde 
+    //Methodes statiques(peu y acceder sans instancier un objet de cette classe) 
+    public static void addSoldeToJson(){
+
+    }
 }
